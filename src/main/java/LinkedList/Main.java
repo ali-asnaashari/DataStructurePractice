eb447c91a7b463de779124fd7822aeb7697125dc
Ali Asnaashari <Ali.Asnaashari2000@gmail.com>
2021-03-14 17:40:59 +0330

    Working With LinkedList

---
+++ src/main/java/LinkedList/Main.java
@@ -0,0 +1,40 @@
+package LinkedList;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /** Part1 - Working With LinkedList */
+
+        // Store bunch of Integer in LinkedList
+        LinkedList<Integer> numbers = new LinkedList<>();
+
+        // Store any kind of object in LinkedList
+        LinkedList list = new LinkedList();
+        list.addFirst(12);
+        list.addLast(20);
+        list.addFirst(10);
+        list.addLast(34);
+        System.out.println(list);
+
+        // contains return boolean
+        System.out.println(list.contains(20));
+
+        // indexOf return index O.W return -1
+        System.out.println(list.indexOf(10));
+        System.out.println(list.indexOf(100));
+
+        // size return number of item in LinkedList
+        System.out.println(list.size());
+
+        list.removeFirst();
+        list.removeLast();
+        System.out.println(list);
+
+        // convert LinkedList to Array
+        Object[] array = list.toArray();
+        System.out.println(Arrays.toString(array));
+    }
+}
