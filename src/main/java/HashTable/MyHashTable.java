e3877eb1c40bc609d807a7a2f880d12dc383a509
Ali Asnaashari <Ali.Asnaashari2000@gmail.com>
2021-04-01 00:44:47 +0430

    Create MyHashTable

---
+++ src/main/java/HashTable/MyHashTable.java
@@ -0,0 +1,63 @@
+package HashTable;
+
+import java.util.LinkedList;
+
+public class MyHashTable {
+    private class Entry{
+        private int key;
+        private String value;
+
+        public Entry(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private int capacity;
+    private LinkedList<Entry>[] entries;
+
+    public MyHashTable(int capacity) {
+        this.capacity = capacity;
+        entries = new LinkedList[capacity];
+    }
+
+    public void put(int key,String value){
+        int index = hash(key);
+
+        if (entries[index] == null)
+            entries[index] = new LinkedList<>();
+
+        for (Entry entry : entries[index]) {
+            if (entry.key == key) {
+                entry.value = value;
+                return;
+            }
+        }
+
+        entries[index].addLast(new Entry(key,value));
+    }
+
+    private int hash(int key){
+        return Math.abs(key) % capacity;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
