95ce5e777085c40a22c8cc96e7bd406174a5ce7c
ali-asnaashari <ali.asnaashari2000@gmail.com>
2021-04-03 20:55:05 +0430

    Implement Tree (insert)

--- src/main/java/Tree/Main.java
+++ src/main/java/Tree/Main.java
@@ -3,6 +3,18 @@ package Tree;
 public class Main {
     public static void main(String[] args) {
 
+        /** Implement Tree (insert) */
+        Tree tree = new Tree();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+        System.out.println("Done!");
+
+
         /** Tree
          * data structure that stores elements in a hierarchy.
          * refer to these elements as nodes.
