e491ac237dc5f6462d30bd387c35a9c3854b3905
ali-asnaashari <ali.asnaashari2000@gmail.com>
2021-04-03 20:51:31 +0430

    working with Tree

---
+++ src/main/java/Tree/Main.java
@@ -0,0 +1,23 @@
+package Tree;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /** Tree
+         * data structure that stores elements in a hierarchy.
+         * refer to these elements as nodes.
+         * lines that connect them as edges.
+         * Each node contains a value or data , even store object.
+         * root : the top node in tree
+         * application: 1)people in our organization 2)DataBase 3)Auto completion 4)compilers -> syntax tree to parse expressions
+         * binary tree = binary search tree -> because allow us to quickly look up data.
+         * In a binary search tree, the value of any node is always greater than the value of leftChild
+         * and smaller than rightChild
+         * logarithmic time:
+         *  lookUp --> o(log n)
+         *  insert --> o(log n)
+         *  delete --> o(log n)
+         */
+
+    }
+}
